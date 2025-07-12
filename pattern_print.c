6873bb42e26598c3f43ad65a72526000325d4ffb
Hrittique_10 <hrittiquesarkarofficial@gmail.com>
2025-07-12 20:57:22 +0600

    Add files via upload

---
+++ pattern_print.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+    
+    
+    int a,b;
+    
+    
+scanf("%d %d",&a,&b);
+
+
+while(1){
+
+if(a==0 && b==0){
+    
+}  
+    break;
+
+    
+    for(int i=0;i<a;i++){
+        for(int j=0; j<b; j++){
+            
+            printf("#\n");
+            
+        }
+    
+        
+        printf("\n");
+        
+    }
+    
+}  
+
+    return 0;
+}
